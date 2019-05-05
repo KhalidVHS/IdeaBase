1facefe0a83b6b3f39c185aa9a65f7a2f034f7a7
Shawn Cameron <35205124+Shawn-Cameron@users.noreply.github.com>
2019-05-04 20:26:11 -0400

    Update mainProd.py

--- mainProd.py
+++ mainProd.py
@@ -1,19 +1,16 @@
 import requests
 
 #The first method is for the pulling of data from the github api...
-URL = "https://developer.uspto.gov/ibd-api/v1/patent/application"
-searchTerm = "cannon"
 
-def retrievingInformation(searching,apiCall):
-    searchtext = searching
-    dataQuery = requests.get(url= apiCall,params={'searchText':searchtext,'start':'0','rows':100})
+def retrievingInformation(searchtext):
+    dataQuery = requests.get(url= "https://developer.uspto.gov/ibd-api/v1/patent/application",params={'searchText':searchtext,'start':'0','rows':100})
     data = dataQuery.json()
     #informationFile = open('information.txt', 'a')
     for x in range(0,len(data['response']['docs'])):
         #informationFile.write(data['response']['docs'][x]['title'])
         print(data['response']['docs'][x]['title'])
 
-retrievingInformation(searchTerm,URL)
+
 
 
 
